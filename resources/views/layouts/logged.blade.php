a2011fce44fc40205e66060074609ed5e1c32507
MarcoNicolodi <marcocontatopro@gmail.com>
2017-08-27 23:55:25 -0300

    ajustes

--- resources/views/layouts/logged.blade.php
+++ resources/views/layouts/logged.blade.php
@@ -46,9 +46,9 @@
         <li class="has-submenu ">
             <a href="#">Minha conta</a>
             <ul class="submenu menu vertical nested " data-submenu>
-                <li><a href="{{url("/logout")}}">Sair</a></li>
-                <li><a>Redefinir senha</a></li>
+                {{--<li disabled><a>Redefinir senha</a></li>--}}
                 <li><a href="{{url("/logs")}}">Logs</a></li>
+                <li><a href="{{url("/logout")}}">Sair</a></li>
             </ul>
         </li>
         </ul>
